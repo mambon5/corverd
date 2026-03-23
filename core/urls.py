0162a400a641dcd3e4016c2c17254f86ddf891db
mambon5 <monantoniet@gmail.com>
2026-03-23 02:12:27 +0100

    entitats i activitats afegides

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('entitats/', views.entitats_list, name='entitats_list'),
+    path('entitat/<int:pk>/activitats/', views.entitat_activitats, name='entitat_activitats'),
+    path('activitats/', views.activitats_calendar, name='activitats_calendar'),
     path('noticia/<int:pk>/', views.noticia_detail, name='noticia_detail'),
     path('manifest/', views.manifest, name='manifest'),
     path('sobre/', views.sobre, name='sobre'),
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Associacio, Noticia, Comentari
+from .models import Associacio, Noticia, Comentari, Activitat
+import json
 
 def index(request):
     return render(request, 'inici.html')
@@ -8,6 +9,31 @@ def entitats_list(request):
     entitats = Associacio.objects.all()
     # Puc afegir les ultimes noticies de cadascuna
     return render(request, 'entitats.html', {'entitats': entitats})
+
+def entitat_activitats(request, pk):
+    entitat = get_object_or_404(Associacio, pk=pk)
+    activitats = entitat.activitats.all()
+    events = []
+    for a in activitats:
+        events.append({
+            'title': a.titol,
+            'start': a.data.strftime('%Y-%m-%d') + 'T' + a.hora.strftime('%H:%M:%S'),
+            'description': a.descripcio,
+        })
+    return render(request, 'entitat_activitats.html', {'entitat': entitat, 'events_json': events})
+
+def activitats_calendar(request):
+    entitats = Associacio.objects.all()
+    activitats = Activitat.objects.all()
+    events = []
+    for a in activitats:
+        events.append({
+            'title': f"[{a.associacio.nom}] {a.titol}",
+            'start': a.data.strftime('%Y-%m-%d') + 'T' + a.hora.strftime('%H:%M:%S'),
+            'description': a.descripcio,
+            'entity_id': a.associacio.id,
+        })
+    return render(request, 'activitats_calendar.html', {'entitats': entitats, 'events_json': events})
 
 def noticia_detail(request, pk):
     noticia = get_object_or_404(Noticia, pk=pk)
