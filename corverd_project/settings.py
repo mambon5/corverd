8ba88a3a55114b03b47dc1d96d4ee8badeb0f2f5
mambon5 <monantoniet@gmail.com>
2026-04-25 11:32:09 +0200

    canviat foto portada

--- corverd_project/settings.py
+++ corverd_project/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
 
